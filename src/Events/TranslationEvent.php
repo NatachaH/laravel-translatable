43e2d7c336425cde7bd2545bcfad906ecf892f8a
Natacha Herth <info@natachaherth.ch>
2021-11-23 11:30:59 +0100

    Corr event

--- src/Events/TranslationEvent.php
+++ src/Events/TranslationEvent.php
@@ -4,22 +4,48 @@ namespace Nh\Translatable\Events;
 
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Nh\Translatable\Models\Translation;
 
 class TranslationEvent
 {
     use Dispatchable, SerializesModels;
 
+    /**
+     * Name of the event
+     * @var string
+     */
     public $name;
+
+    /**
+     * The model parent of the translation
+     * @var \Illuminate\Database\Eloquent\Model
+     */
     public $model;
+
+    /**
+     * The translation model
+     * @var \Nh\Translatable\Models\Translation
+     */
+    public $relation;
+
+    /**
+     * The number of translation model affected
+     * @var int
+     */
+    public $number;
 
     /**
      * Create a new event instance.
      * @param string  $name
      * @param \Illuminate\Database\Eloquent\Model  $model
+     * @param \Nh\Translatable\Models\Translation  $relation
+     * @param int  $number
      */
-    public function __construct($name,$model)
+    public function __construct($name,$model,$relation = null,$number = null)
     {
-          $this->name    = 'translation.'.$name;
-          $this->model   = $model;
+          $this->name     = $name;
+          $this->model    = $model;
+          $this->relation = is_null($relation) ? new Translation : $relation;
+          $this->number   = $number;
     }
 }
